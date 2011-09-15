3249be06ebda610d670971c9259e65439b83ceae
finn <finn.kuusisto@gmail.com>
2011-09-14 22:44:12 -0500

    modified test code to run on all latin files

--- src/kuusisto/finn/phi/test/TestExtractor.java
+++ src/kuusisto/finn/phi/test/TestExtractor.java
@@ -1,10 +1,10 @@
 package kuusisto.finn.phi.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,50 +13,65 @@ import kuusisto.finn.phi.Record;
 public class TestExtractor {
 
 	public static void main(String[] args) {
-		FileInputStream in = null;
-		FileChannel inFC = null;
-		long numBytes = 0;
-		try {
-			in = new FileInputStream("./data/lat0656.txt");
-			inFC = in.getChannel();
-			numBytes = inFC.size();
-		} catch (FileNotFoundException e) {
-			System.err.println("Couldn't open file!");
-			e.printStackTrace();
-			return;
-		} catch (IOException e) {
-			System.err.println("Couldn't read byte count!");
-			e.printStackTrace();
-			return;
-		}
+		File dataDir = new File("./data/");
+		File[] files = dataDir.listFiles();
 		PrintWriter out = null;
 		try {
 			out = new PrintWriter("./output/text.output");
-			byte[] curr = {-128};
-			in.read(curr);
-			for (long i = 1; i < numBytes;) {
-				List<Byte> idBuffer = new ArrayList<Byte>();
-				while (TestUtil.isBitSet(curr[0], 7) && i < numBytes) {
-					idBuffer.add(curr[0]);
-					in.read(curr);
-					i++;
+			for (File file : files) {
+				if (!file.isFile() || !file.getName().startsWith("lat")) {
+					continue;
 				}
-				List<Byte> textBuffer = new ArrayList<Byte>();
-				while (!TestUtil.isBitSet(curr[0], 7) && i < numBytes) {
-					textBuffer.add(curr[0]);
-					in.read(curr);
-					i++;
+				//valid file, read it
+				FileInputStream in = null;
+				try {
+					in = new FileInputStream(file);
+					out.println("*****************************");
+					out.println(file.getName());
+					out.println("*****************************");
+					TestExtractor.readFile(in, out);
 				}
-				//convert those suckers
-				Record rec = new Record(idBuffer, textBuffer);
-				System.out.println(rec);
+				finally {
+					in.close();
+				}
 			}
-		} catch (IOException e) {
-			System.err.println("Error reading!");
+		}
+		catch (FileNotFoundException e) {
+			System.err.println("Couldn't open file!");
 			e.printStackTrace();
 			return;
-		} finally {
+		}
+		catch (IOException e) {
+			System.err.println("Failed closing input file!");
+			e.printStackTrace();
+			return;
+		}
+		finally {
 			out.close();
+		}
+	}
+	
+	private static void readFile(FileInputStream in, PrintWriter out)
+			throws IOException {
+		long numBytes = in.getChannel().size();
+		byte[] curr = {-128};
+		in.read(curr);
+		for (long i = 1; i < numBytes;) {
+			List<Byte> idBuffer = new ArrayList<Byte>();
+			while (TestUtil.isBitSet(curr[0], 7) && i < numBytes) {
+				idBuffer.add(curr[0]);
+				in.read(curr);
+				i++;
+			}
+			List<Byte> textBuffer = new ArrayList<Byte>();
+			while (!TestUtil.isBitSet(curr[0], 7) && i < numBytes) {
+				textBuffer.add(curr[0]);
+				in.read(curr);
+				i++;
+			}
+			//convert those suckers
+			Record rec = new Record(idBuffer, textBuffer);
+			out.println(rec.toString());
 		}
 	}
 	
